f45942d9d639babbc95314f71b5b52973c268a74
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-09-11 20:06:38 +0530

    10.9.23.3

---
+++ JAVAPRACTISE/FinalEx.java
@@ -0,0 +1,26 @@
+package JAVAPRACTISE;
+
+public class FinalEx 
+{
+    private final int num1=10;
+    private final int num2;
+
+    public FinalEx(int num2)
+    {
+        this.num2=num2;
+    }
+    public int getnum1()
+    {
+        return num1;
+    }
+    public int getnum2()
+    {
+        return num2;
+    }
+    public static void main(String[] args)
+    {
+        FinalEx e1=new FinalEx(20);
+        System.out.println(e1.getnum1()+" "+e1.getnum2());
+    }
+    
+}
---
+++ JAVAPRACTISE/FinalEx_Person.java
@@ -0,0 +1,33 @@
+package JAVAPRACTISE;
+
+public class FinalEx_Person 
+{
+    private final int aadhar;
+    private final int pan;
+    private final String name;
+    private int age;
+    
+    public FinalEx_Person(int a,int p,String n,int age)
+    {
+        this.aadhar=a;
+        this.pan=p;
+        this.name=n;
+        this.age=age;
+    }
+
+    public int getaadhar()
+    {return aadhar;}
+    public int getpan()
+    {return pan;}
+    public String getname()
+    {return name;}
+    public int getage()
+    {return age;}
+
+    public static void main(String[] args)
+    {
+        FinalEx_Person p =new FinalEx_Person(123456789, 1001, "Aalok", 19);
+        System.out.println(p.getname()+" "+p.getage()+" "+p.getaadhar()+" "+p.getpan());
+    }
+
+}
