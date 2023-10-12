60bac07924570f7579752d5b826ff9b92e02d7f3
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-10-12 23:37:44 +0530

    Aalok.12/10/23.23/38

---
+++ JAVAPRACTISE/BtoI.java
@@ -0,0 +1,11 @@
+public class BtoI {
+    public static void main(String[] args) {
+        String a="11";
+        String b="1";
+        int a1=Integer.parseInt(a,2);
+        int b1=Integer.parseInt(b,2);
+        int c1=a1+b1;
+        String s=Integer.toBinaryString(10);
+        System.out.println(s);
+    }    
+}
