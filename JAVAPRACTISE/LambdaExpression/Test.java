d9e3e59f7ff67d1f90f2983298c1dcee1af5022e
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-10-27 19:32:04 +0530

    AalokChoudhari1020

--- JAVAPRACTISE/LambdaExpression/Test.java
+++ JAVAPRACTISE/LambdaExpression/Test.java
@@ -4,13 +4,13 @@ import packinterface.Arithmatic;
 
 public class Test {
     public static void main(String[] args) {
-        // Arithmatic ar=new Arithmatic() {
-        //     public void sum(int a,int b)
-        //     {
-        //         System.out.println(a+b);
-        //     }
-        // };
-        Arithmatic ar=(int a,int b)->System.out.println(a+b);
-        ar.Sum(10, 12);
+        Arithmatic ar=new Arithmatic() {
+            public void sum(int a,int b)
+            {
+                System.out.println(a+b);
+            }
+        };
+        // Arithmatic ar=(int a,int b)->System.out.println(a+b);
+        // ar.Sum(10, 12);
     }    
 }
