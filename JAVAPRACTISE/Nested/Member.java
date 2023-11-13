6480fa796c32007fe7155fe00eeaf02cc6a92edf
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-11-13 23:14:17 +0530

    aalokk

--- JAVAPRACTISE/Nested/Member.java
+++ JAVAPRACTISE/Nested/Member.java
@@ -8,6 +8,8 @@ public class Member {
             {
                 System.out.println("Hi this is member class");
                 System.out.println("Welcome");
+                System.out.println("What is your name");
+                
             }
         }
         Minner m=new Minner();
