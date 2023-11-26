c7d304a301a64ad979db9e2194a7def5cb98c055
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-11-26 20:19:05 +0530

    aalok

--- JAVAPRACTISE/VIPCAFE.java
+++ JAVAPRACTISE/VIPCAFE.java
@@ -2,13 +2,7 @@ import java.util.*;
 public class VIPCAFE {
     public static int Findmax(int[] a)
     {
-        int max=0,index=0;
-        for(int i=0;i<a.length;i++)
-        {
-            if(a[i]>max)
-            {max=a[i];index=i;}
-        }
-        return index+1;
+        
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -21,8 +15,13 @@ public class VIPCAFE {
         int k=sc.nextInt();//friends position
         while(true)
         {
-            int maxi=Findmax(a);
-            if(maxi==k){System.out.println(1);}
+            int max=0,index=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]>max)
+            {max=a[i];index=i;}
+        }
+            if(index==k){System.out.println(1);}
             else
             {
                 System.out.println(k++);
