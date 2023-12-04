92308d585b2a8658b3a9b78ec3f5cea565748cbe
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-12-04 19:16:39 +0530

    aalok

--- JAVAPRACTISE/BuilderPattern/ArbiMethodRefrence.java
+++ JAVAPRACTISE/BuilderPattern/ArbiMethodRefrence.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ArbiMethodRefrence {
     public static void main(String[] args) {
         List<Integer>numbers=Arrays.asList(10,4,-1,5,2,-6,-3,-10);
-        numbers.sort(Integer::compareTo);
+        numbers.sort(Integer::compareTo);//inbuilt method
         numbers.forEach(System.out::println);
     }
 }
