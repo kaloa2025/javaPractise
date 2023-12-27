e1fe6f90fe7c8e11ab415da01b734e971303bc6d
kaloa2025 <aalokchoudhari2021@gmail.com>
2023-12-27 20:55:18 +0530

    aalokzcf

--- JAVAPRACTISE/zCF/Year.java
+++ JAVAPRACTISE/zCF/Year.java
@@ -46,7 +46,7 @@ public class Year {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=n+1;i<9000;i++)
+        for(int i=n+1;i<9000000;i++)
         {
             if(hasduplicates(i))
             {
