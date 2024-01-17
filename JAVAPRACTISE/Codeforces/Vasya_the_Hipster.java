e3ca3656adf67487a19ae39eefdafed0515337af
kaloa2025 <aalokchoudhari2021@gmail.com>
2024-01-17 23:51:19 +0530

    Vasya the hipster

---
+++ JAVAPRACTISE/Codeforces/Vasya_the_Hipster.java
@@ -0,0 +1,23 @@
+package Codeforces;
+import java.util.*;
+public class Vasya_the_Hipster{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if (a >= b)
+    {
+        a -= b;
+        a /= 2;
+        System.out.println(b+" "+a);
+    }
+    else
+    {
+        b -= a;
+        b /= 2;
+        System.out.println(a+" "+b);
+    }
+    }
+}
+}
